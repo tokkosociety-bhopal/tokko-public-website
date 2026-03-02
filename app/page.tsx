5e7df47c8ada6636e55fdcdb02cbe5db5aea5cc7
tokkosociety-wq <tokkosociety@gmail.com>
2026-03-02 19:32:17 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,32 @@
 "use client";
 
 import Hero from "./components/Hero";
-import StatsSection from "./components/StatsSection";
-import HowItWorks from "./components/HowItWorks";
-import ProblemSolution from "./components/ProblemSolution";
-import DetailedFeatures from "./components/DetailedFeatures";
+import Stats from "./components/Stats";
 import Features from "./components/Features";
 import Security from "./components/Security";
-import Testimonials from "./components/Testimonials";
-import FAQ from "./components/FAQ";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
+import DetailedFeatures from "./components/DetailedFeatures";
+import ProblemSolution from "./components/ProblemSolution";
+import StatsSection from "./components/StatsSection";
+import HowItWorks from "./components/HowItWorks";
+import Testimonials from "./components/Testimonials";
+import FAQ from "./components/FAQ";
 
 export default function Home() {
   return (
     <main className="bg-white text-gray-800">
-
-      {/* HERO */}
       <Hero />
-
-      {/* TRUST NUMBERS */}
       <StatsSection />
-
-      {/* HOW IT WORKS */}
       <HowItWorks />
-
-      {/* PROBLEM / SOLUTION */}
       <ProblemSolution />
-
-      {/* DETAILED FEATURES */}
       <DetailedFeatures />
-
-      {/* FEATURE GRID */}
       <Features />
-
-      {/* SECURITY SECTION */}
       <Security />
-
-      {/* TESTIMONIALS */}
       <Testimonials />
-
-      {/* FAQ */}
       <FAQ />
-
-      {/* CALL TO ACTION */}
       <CTA />
-
-      {/* FOOTER */}
       <Footer />
-
     </main>
   );
 }
